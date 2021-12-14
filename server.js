94aacfd40359675e3cbf7e90df307ed4296470fa
Pelle Lundgren <pellburt@gmail.com>
2021-12-14 07:56:20 +0100

    Add rudimentary response

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  // Set header content type for the response
+  res.setHeader("Content-Type", "text/plain");
+  res.write("Hello there my friend.");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
