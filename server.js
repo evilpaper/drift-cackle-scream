dce8cb660357c73f9c63c64a48df46e715a13a54
Pelle Lundgren <pellburt@gmail.com>
2021-12-15 07:14:54 +0100

    Add views folder and serve html

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   // Set header content type for the response
-  res.setHeader("Content-Type", "text/plain");
-  res.write("Hello there my friend.");
-  res.end();
+  res.setHeader("Content-Type", "text/html");
+
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
