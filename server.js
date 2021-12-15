91ec5f85f02f5ab58557033fbcb1eeadf7b9793f
Pelle Lundgren <pellburt@gmail.com>
2021-12-15 07:26:45 +0100

    Add basic routing system

--- server.js
+++ server.js
@@ -7,7 +7,21 @@ const server = http.createServer((req, res) => {
   // Set header content type for the response
   res.setHeader("Content-Type", "text/html");
 
-  fs.readFile("./views/index.html", (err, data) => {
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
     } else {
