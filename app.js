fbd28e0c5cfa3211b84e2a7865b6b68be45a9890
Pelle Lundgren <pellburt@gmail.com>
2021-12-25 08:22:41 +0100

    Move server.js functionality to app.js and using express

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.listen(3000);
+
 app.get("/", function (req, res) {
-  res.send("Hello from Express!");
+  res.sendFile("./views/index.html", { root: __dirname });
 });
 
-app.listen(3000);
+app.get("/about", function (req, res) {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+// redirect
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
